3fd34a2c2e71a59c63672e4c1faff40675fc7d68
Kurt Maglalang <kbm126@live.com>
2023-09-13 21:20:13 -0700

    feat: update call to action with website info

--- src/components/features/HomepageCallToAction.tsx
+++ src/components/features/HomepageCallToAction.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Link from "next/link";
+
+import { CTA_PITCH } from "@/constants/staticText";
 
 function HomepageCallToAction() {
   return (
@@ -6,12 +9,13 @@ function HomepageCallToAction() {
       <div className="container px-5 py-24 mx-auto">
         <div className="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
           <h1 className="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900">
-            Slow-carb next level shoindxgoitch ethical authentic, scenester
-            sriracha forage.
+            {CTA_PITCH}
           </h1>
-          <button className="flex-shrink-0 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-10 sm:mt-0">
-            Button
-          </button>
+          <Link href={"/signup"}>
+            <button className="flex-shrink-0 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-10 sm:mt-0">
+              Sign Up
+            </button>
+          </Link>
         </div>
       </div>
     </section>
