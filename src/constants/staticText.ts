13b8cd81b0373e53f22b8689f5b57763eb11412a
Kurt Maglalang <kbm126@live.com>
2023-09-20 12:24:42 -0700

    style: text styling update

--- src/constants/staticText.ts
+++ src/constants/staticText.ts
@@ -1,48 +1,54 @@
 // header
-export const COMPANY_NAME = 'ConsistChart'
+export const COMPANY_NAME = "ConsistChart";
 
 // footer
-export const FACEBOOK_LINK = 'https://www.facebook.com'
-export const TWITTER_LINK = 'https://www.twitter.com'
-export const INSTAGRAM_LINK = 'https://www.instagram.com'
-export const LINKEDIN_LINK = 'https://www.linkedin.com'
+export const FACEBOOK_LINK = "https://www.facebook.com";
+export const TWITTER_LINK = "https://www.twitter.com";
+export const INSTAGRAM_LINK = "https://www.instagram.com";
+export const LINKEDIN_LINK = "https://www.linkedin.com";
 
 // hero
-export const HERO_TITLE = 'Visualize Progress. Build Habits'
-export const HERO_DESC = 'Unlock the power of habit formation with a visual representation of your journey. Each dot signifies a commitment, a day conquered, a step closer to your goal. With ConsistChart, let every consistent effort be celebrated and magnified'
+export const HERO_TITLE = "Visualize Progress. Build Habits";
+export const HERO_DESC =
+  "Unlock the power of habit formation with a visual representation of your journey. Each dot signifies a commitment, a day conquered, a step closer to your goal. With ConsistChart, let every consistent effort be celebrated and magnified";
 
 // call to action
-export const CTA_PITCH = 'Ready to visualize your path to success? Join ConsistChart today and let every step of your journey shine brightly'
+export const CTA_PITCH =
+  "Ready to visualize your path to success? Join ConsistChart today and let every step of your journey shine brightly";
 
 // feature section
-export const FEATURE_TITLE = "Empower Your Habits with Our Key Features"
+export const FEATURE_TITLE = "Empower Your Habits with Our Key Features";
 export const FEATURE_BENEFIT_1 = {
-  title: 'Dynamic Visualization',
-  description: `Witness real-time progress as your habits unfold, turning every day's dedication into a vibrant piece of your success mosaic.`
-}
+  title: "Dynamic Visualization",
+  description: `Witness real-time progress as your habits unfold, turning every day's dedication into a vibrant piece of your success mosaic.`,
+};
 export const FEATURE_BENEFIT_2 = {
-  title: 'Tailored Tracking',
-  description: `Customize and categorize your habits, ensuring that each unique goal is monitored and celebrated in its own right.`
-}
+  title: "Tailored Tracking",
+  description: `Customize and categorize your habits, ensuring that each unique goal is monitored and celebrated in its own right.`,
+};
 export const FEATURE_BENEFIT_3 = {
-  title: 'Motivational Metrics',
-  description: `Dive deep into your performance analytics, gaining insights that inspire and guide you to maintain consistency and reach new heights.`
-}
+  title: "Motivational Metrics",
+  description: `Dive deep into your performance analytics, gaining insights that inspire and guide you to maintain consistency and reach new heights.`,
+};
 
 // content steps
 export const CONTENT_STEP_1 = {
-  title: 'Create Your Graph',
-  description: 'Initiate your journey by crafting a new graph chart, inspired by the familiar GitHub commit design.'
-}
+  title: "Create Your Graph",
+  description:
+    "Initiate your journey by crafting a new graph chart, inspired by the familiar GitHub commit design.",
+};
 export const CONTENT_STEP_2 = {
-  title: 'Log Your Achievement',
-  description: 'Record each task accomplishment, ensuring every effort is recognized on your habit journey.'
-}
+  title: "Log Your Achievement",
+  description:
+    "Record each task accomplishment, ensuring every effort is recognized on your habit journey.",
+};
 export const CONTENT_STEP_3 = {
-  title: 'Immediate Updates',
-  description: 'As you log, see real-time reflections on your heatmap, making every commitment shine.'
-}
+  title: "Immediate Updates",
+  description:
+    "As you log, see real-time reflections on your heatmap, making every commitment shine.",
+};
 export const CONTENT_STEP_4 = {
-  title: 'Watch and Grow',
-  description: 'Observe the fruits of your persistence. As days turn into weeks, watch your progress tell its own story.'
-}
+  title: "Watch and Grow",
+  description:
+    "Observe the fruits of your persistence. As days turn into weeks, watch your progress tell its own story.",
+};
