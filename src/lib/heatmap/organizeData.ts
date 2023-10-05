5ce81432392930b759f3f698d632ff2641747d1d
Kurt Maglalang <kbm126@live.com>
2023-10-04 21:41:00 -0700

    feat: helper functions for the heatmaps

---
+++ src/lib/heatmap/organizeData.ts
@@ -0,0 +1,36 @@
+import { DAYS } from "@/constants/heatmapText";
+
+type DateDocument = {
+  createdAt: string;
+  dayOfWeek: string;
+  description: string;
+  title: string;
+  weekOfYear: number;
+};
+
+export const organizeDateData = (documents: DateDocument[]) => {
+  // Initial structure
+  const grouped: { [day: string]: { [week: number]: number } } = {};
+  DAYS.forEach((day) => {
+    grouped[day] = {};
+  });
+
+  // Group by dayOfWeek and weekOfYear
+  documents.forEach((doc) => {
+    if (!grouped[doc.dayOfWeek][doc.weekOfYear]) {
+      grouped[doc.dayOfWeek][doc.weekOfYear] = 0;
+    }
+    grouped[doc.dayOfWeek][doc.weekOfYear]++;
+  });
+
+  // Create the series
+  const series = DAYS.map((day) => {
+    const data = Array(52).fill(0);
+    for (const week in grouped[day]) {
+      data[parseInt(week) - 1] = grouped[day][parseInt(week)];
+    }
+    return { name: day, data };
+  });
+
+  return series;
+};
