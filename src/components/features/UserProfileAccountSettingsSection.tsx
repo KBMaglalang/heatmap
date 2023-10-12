d48e0e7757b2e4db5985ad73cf88c8587a93574b
Kurt Maglalang <kbm126@live.com>
2023-10-12 07:48:00 -0700

    feat: update the styling of the update button

--- src/components/features/UserProfileAccountSettingsSection.tsx
+++ src/components/features/UserProfileAccountSettingsSection.tsx
@@ -59,7 +59,12 @@ export default function UserProfileAccountSettingsSection() {
             onChange={(e) => setName(e.target.value)}
           />
 
-          <button onClick={handleUpdateName}>Update</button>
+          <button
+            className="inline-flex items-center bg-brand-interface border-0 py-1 px-3 focus:outline-none hover:bg-indigo-600 rounded text-base mt-4 md:mt-0 mr-5 text-white font-brand-montserrat font-regular"
+            onClick={handleUpdateName}
+          >
+            Update
+          </button>
         </div>
       </div>
     </div>
