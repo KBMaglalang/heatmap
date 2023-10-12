bef9ae9921fcb6761ed4434fef0eadf6c192b616
Kurt Maglalang <kbm126@live.com>
2023-10-11 20:54:51 -0700

    feat: notification toggle updates state on firestore

--- src/components/features/UserProfileNotificationSection.tsx
+++ src/components/features/UserProfileNotificationSection.tsx
@@ -1,13 +1,38 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+
+import { db } from "../../../firebase";
 
 export default function UserProfileNotificationSection() {
+  const { data: session } = useSession();
   const [notificationsToggle, setNotificationsToggle] = useState(false);
 
-  const handleNotificationToggle = () => {
-    // toast.success("Notifications updated");
+  useEffect(() => {
+    if (session) {
+      const fetchAndSetUserData = async () => {
+        // Define a reference to the user document in Firestore
+        const userRef = doc(db, "users", session?.user?.email!);
+
+        // Fetch the user document
+        const userDoc = await getDoc(userRef);
+
+        setNotificationsToggle(userDoc?.data()?.notification);
+      };
+
+      fetchAndSetUserData();
+    }
+  }, [session]);
+
+  const handleNotificationToggle = async () => {
     setNotificationsToggle(!notificationsToggle);
+
+    const userRef = doc(db, "users", session?.user?.email!);
+    await updateDoc(userRef, {
+      notification: !notificationsToggle,
+    });
   };
 
   return (
@@ -25,9 +50,9 @@ export default function UserProfileNotificationSection() {
         <label className="relative inline-flex items-center cursor-pointer">
           <input
             type="checkbox"
-            value=""
+            checked={notificationsToggle}
             className="sr-only peer"
-            onClick={handleNotificationToggle}
+            onChange={handleNotificationToggle}
           />
           <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
 
