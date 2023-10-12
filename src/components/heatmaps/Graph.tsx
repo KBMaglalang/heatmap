3f569a4f8eb20318faa12b93141debe03d3cb94e
Kurt Maglalang <kbm126@live.com>
2023-10-12 11:07:34 -0700

    feat: set the graph to have adjustable width

--- src/components/heatmaps/Graph.tsx
+++ src/components/heatmaps/Graph.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import dynamic from "next/dynamic";
 const ApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
@@ -14,14 +14,41 @@ export default function Graph({ commitsData }: Props) {
   const heatmapData = commitsData?.docs?.map((commit: any) => commit.data());
   const seriesData = organizeDateData(heatmapData);
 
+  // State to manage chart width
+  const [chartWidth, setChartWidth] = useState(0);
+
+  // Reference to the container
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    // Function to update width based on container size
+    const updateChartWidth = () => {
+      if (containerRef.current) {
+        setChartWidth(containerRef.current);
+      }
+    };
+
+    // Initial call to set width
+    updateChartWidth();
+
+    // Attach window resize event listener
+    window.addEventListener("resize", updateChartWidth);
+
+    // Cleanup event listener on component unmount
+    return () => window.removeEventListener("resize", updateChartWidth);
+  }, []);
+
   return (
-    <div className="relative flex flex-col items-center mx-auto px-4 my-4 z-10">
+    <div
+      ref={containerRef}
+      className="relative flex flex-col items-center mx-auto px-4 my-4 z-10"
+    >
       <ApexChart
         type="heatmap"
         options={CHART_OPTIONS}
         series={seriesData}
         height={400}
-        width={1500}
+        width={chartWidth}
       />
     </div>
   );
