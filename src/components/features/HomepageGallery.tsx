956387dd90fc0f8817c8811f101e45fa83e1b1c6
Kurt Maglalang <kbm126@live.com>
2023-10-17 12:46:46 -0700

    feat: use the gally item in the homepage gallery section

--- src/components/features/HomepageGallery.tsx
+++ src/components/features/HomepageGallery.tsx
@@ -1,5 +1,13 @@
 import React from "react";
-import Image from "next/image";
+
+import {
+  GALLERY_TITLE,
+  GALLERY_DESC,
+  GALLERY_LIST,
+} from "@/constants/staticText";
+
+// components
+import GalleryItem from "../common/GalleryItem";
 
 function HomepageGallery() {
   return (
@@ -7,92 +15,21 @@ function HomepageGallery() {
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-col text-center w-full mb-20">
           <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-brand-black font-brand-roboto ">
-            Visual Empowerment for Habit Mastery
+            {GALLERY_TITLE}
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base font-brand-montserrat font-regular">
-            Discover the transformative impact of visual tracking. Our unique
-            heatmap representation captures your dedication and progress,
-            turning your daily efforts into a vibrant tapestry of achievement.
-            Stay inspired, stay consistent, and watch your goals come to life.
+            {GALLERY_DESC}
           </p>
         </div>
         <div className="flex flex-wrap -m-4">
-          <div className="lg:w-1/3 sm:w-1/2 p-4">
-            <div className="flex relative">
-              <Image
-                src={"/assets/images/g-1.jpg"}
-                alt="gallery"
-                style={{ objectFit: "cover", objectPosition: "center" }}
-                className="absolute inset-0 w-full h-full object-cover object-center rounded-lg img-fluid"
-                width={500}
-                height={500}
-              />
-              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                {/* <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                  THE SUBTITLE
-                </h2> */}
-                <h1 className="title-font text-lg font-medium text-brand-black mb-3 font-brand-montserrat ">
-                  Master Your Momentum
-                </h1>
-                <p className="leading-relaxed font-brand-montserrat font-regular">
-                  Unlock the power of visual feedback with Consistency
-                  Visualizer. As you see your efforts mapped out day by day,
-                  harness the motivation to build, maintain, and elevate your
-                  daily commitments.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="lg:w-1/3 sm:w-1/2 p-4">
-            <div className="flex relative">
-              <Image
-                src={"/assets/images/g-2.jpg"}
-                alt="gallery"
-                style={{ objectFit: "cover", objectPosition: "center" }}
-                className="absolute inset-0 w-full h-full object-cover object-center img-fluid rounded-lg"
-                width={500}
-                height={500}
-              />
-              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                {/* <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                  THE SUBTITLE
-                </h2> */}
-                <h1 className="title-font text-lg font-medium text-brand-black mb-3 font-brand-montserrat">
-                  Consistency, Color-Coded
-                </h1>
-                <p className="leading-relaxed font-brand-montserrat font-regular">
-                  Dive deep into a colorful representation of your daily drive.
-                  With each hue, recognize your dedication, and be inspired to
-                  add more to your canvas.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="lg:w-1/3 sm:w-1/2 p-4">
-            <div className="flex relative">
-              <Image
-                src={"/assets/images/g-3.jpg"}
-                alt="gallery"
-                style={{ objectFit: "cover", objectPosition: "center" }}
-                className="absolute inset-0 w-full h-full object-cover object-center img-fluid rounded-lg"
-                width={500}
-                height={500}
-              />
-              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                {/* <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                  THE SUBTITLE
-                </h2> */}
-                <h1 className="title-font text-lg font-medium text-brand-black mb-3 font-brand-montserrat">
-                  Visualize to Actualize
-                </h1>
-                <p className="leading-relaxed font-brand-montserrat font-regular">
-                  Every goal starts with consistent effort. Watch your
-                  persistence unfold in a dynamic heatmap, propelling you to
-                  commit, continue, and conquer.
-                </p>
-              </div>
-            </div>
-          </div>
+          {GALLERY_LIST.map((item, index) => (
+            <GalleryItem
+              key={index}
+              title={item.title}
+              description={item.description}
+              image={item.image}
+            />
+          ))}
         </div>
       </div>
     </section>
