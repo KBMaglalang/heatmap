cedda42f1270809558f6baf92fb12b11d544f95e
Kurt Maglalang <kbm126@live.com>
2023-10-17 12:43:49 -0700

    feat: move user components to a user folder

--- src/components/user/UserLayout.tsx
+++ src/components/user/UserLayout.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 // components
-import Header from "../Header";
-import Footer from "../Footer";
+import Header from "../features/Header";
+import Footer from "../features/Footer";
 
 type Props = {
   children: React.ReactNode;
