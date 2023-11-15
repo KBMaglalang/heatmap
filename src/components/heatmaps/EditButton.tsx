6a3cd6a3872a46230618cd13c66eafb31a6126af
Kurt Maglalang <kbm126@live.com>
2023-11-14 17:32:51 -0800

    feat: set the graph to have overflow scroll

--- src/components/heatmaps/EditButton.tsx
+++ src/components/heatmaps/EditButton.tsx
@@ -14,7 +14,7 @@ export default function EditButton({ heatmapDoc, callback }: Props) {
   const [showModal, setShowModal] = useState(false);
 
   return (
-    <div>
+    <div className="">
       <div className="cursor-pointer" onClick={() => setShowModal(true)}>
         <WrenchIcon className="h-6 w-6 text-gray-500 hover:text-gray-700 " />
       </div>
--- src/components/heatmaps/LoadingSpinner.tsx
+++ src/components/heatmaps/LoadingSpinner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function LoadingSpinner() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="w-16 h-16 border-t-4 border-blue-500 border-solid rounded-full animate-spin"></div>
+      <span className="loading loading-dots loading-lg"></span>
     </div>
   );
 }
